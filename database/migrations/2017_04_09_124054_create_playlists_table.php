90689038ebdb942ade75e6828757414b3e369d30
freek <freek@spatie.be>
2017-04-09 14:43:34 +0200

    add playlists migration

---
+++ database/migrations/2017_04_09_124054_create_playlists_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlaylistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('playlists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('text');
+            $table->datetime('publish_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('playlists');
+    }
+}
