fe8db53151f0bbf73a8ab3af27ab6ec6bf384305
freek <freek@spatie.be>
2017-04-09 17:22:32 +0200

    add actions to playlist index

--- resources/views/playlists/index.blade.php
+++ resources/views/playlists/index.blade.php
@@ -1,30 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('_partials.flashMessage')
 
     <h1>Playlists</h1>
 
-    <table>
+    @include('_partials.flashMessage')
+
+    <a class="btn btn-primary" href="{{ action('PlaylistsController@create') }}">New item</a>
+
+    <table class="table">
         <thead>
-            <tr>
-                <th>Name</th>
-                <th>Text</th>
-                <th>Publish date</th>
-                <th>User</th>
-                <th></th>
-            </tr>
+        <th>Name</th>
+        <th></th>
         </thead>
         <tbody>
-            @foreach($playlists as $playlist)
-                <tr>
-                    <td>{{ $playlist->name }}</td>
-                    <td>{{ $playlist->excerpt }}</td>
-                    <td>{{ $playlist->publish_date->format('d/m/Y') }}</td>
-                    <td>{{ $playlist->user->name }}</td>
-                    <td>@include('_partials.deleteButton', ['url' => action('PlaylistsController@destroy', [$playlist->id])])</td>
-                </tr>
-            @endforeach
+
+        @foreach($playlists as $playlist)
+            <tr>
+                <td><a href="{{ action('PlaylistsController@edit', [$playlist->id]) }}">{{ $playlist->name }}</a></td>
+                <td>@include('_partials.deleteButton', ['url' => action('PlaylistsController@destroy', [$playlist->id])])</td>
+            </tr>
+        @endforeach
         </tbody>
     </table>
 @endsection
