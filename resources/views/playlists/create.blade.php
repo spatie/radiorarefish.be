3609e36d3924c8ee0d0a62604b7b9f75a9c12e96
freek <freek@spatie.be>
2017-04-09 17:26:14 +0200

    fix title tag

--- resources/views/playlists/create.blade.php
+++ resources/views/playlists/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h1>New playlist/h1>
+    <h1>New playlist</h1>
 
     <form action="{{ action('PlaylistsController@store') }}" method="POST">
         @include('playlists._partials.form', ['submitText' => 'Create'])
