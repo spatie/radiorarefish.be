69649fcadf2f3ce705a944dadcb6213510c170c1
freek <freek@spatie.be>
2017-04-09 20:26:00 +0200

    improve search page

--- resources/views/front/search/index.blade.php
+++ resources/views/front/search/index.blade.php
@@ -2,20 +2,24 @@
 
 @section('content')
 
-    <h1>Search playlists</h1>
-
     <div clas="row">
         @include('front._partials.searchForm')
     </div>
 
+    <h1>Search results for "{{ $query }}"</h1>
+
     <hr>
 
     @if(! empty($query))
-        <h4>Results for "{{ $query }}"</h4>
-
         @forelse($playlists as $playlist)
             <div class="row">
-                {{ $playlist->name }}
+                <h2>
+                    <a href="{{ action('Front\PlaylistsController@detail', [$playlist->slug]) }}">
+                        {{ $playlist->name }}
+                    </a>
+                </h2>
+
+                {!! $playlist->text !!}
             </div>
         @empty
             <div class="alert alert-warning" role="alert">No playlists found</div>
