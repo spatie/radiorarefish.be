9e41b01c7ac57de3153c06777e1845552ddf3122
freek <freek@spatie.be>
2017-04-09 20:35:37 +0200

    add custom 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('front.layouts.app')
+
+@section('content')
+    <div class="alert alert-danger" role="alert">I'm not here...</div>
+
+    <div class="row">
+        <div class="text-center">
+            <iframe width="560" height="315"
+                    src="https://www.youtube.com/embed/nZq_jeYsbTs?rel=0&amp;controls=0&amp;showinfo=0?ecver=1"
+                    frameborder="0" allowfullscreen>
+            </iframe>
+        </div>
+    </div>
+@endsection
