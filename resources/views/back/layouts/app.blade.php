911087f5052bd39aca2659086dad59da3cd4134a
freek <freek@spatie.be>
2017-04-09 21:06:19 +0200

    fix template title

--- resources/views/back/layouts/app.blade.php
+++ resources/views/back/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Radio Rarefish</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
