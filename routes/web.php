7f46464e032d539f5df91769a9edd4cce8ee3269
freek <freek@spatie.be>
2017-04-09 13:52:09 +0200

    move auth to routes

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', 'HomeController@index');
+});
