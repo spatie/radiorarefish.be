ece0c1fa6c02a49562ccbd43653af340ad6c359c
freek <freek@spatie.be>
2017-04-09 19:06:29 +0200

    add glyphicons

--- resources/views/front/playlists/detail.blade.php
+++ resources/views/front/playlists/detail.blade.php
@@ -1,6 +1,7 @@
 @extends('front.layouts.app')
 
 <a href="{{ action('Front\PlaylistsController@index') }}">
+    <span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span>
     Back to list
 </a>
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,6 @@ const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/', 'public/fonts/bootstrap');
+
