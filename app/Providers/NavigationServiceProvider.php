79fa488261367dbdc7e52479272f45da5fd0839b
freek <freek@spatie.be>
2017-04-09 20:56:12 +0200

    add navigation service provider

---
+++ app/Providers/NavigationServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class NavigationServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+}
