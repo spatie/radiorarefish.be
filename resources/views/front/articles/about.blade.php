2f85747c61a298dac83dbe9512d28c046d52c7e3
freek <freek@spatie.be>
2017-04-11 00:58:54 +0200

    add about page

--- resources/views/front/articles/about.blade.php
+++ resources/views/front/articles/about.blade.php
@@ -4,6 +4,17 @@
 
     <h1>About</h1>
 
-    Coming soon...
+    <p>
+        Radio Rarefish is a monthly Radioshow where you can listen to cool new and old music. The style of played tracks
+        vary wildly, but there is an emphasis on krautrock, electronic and minimal music. The show is broadcasted every second saturday of the month at 11 AM (GMT+1) on the
+        local station <a href="http://www.radiocentraal.be/Realescape/">Radio Centraal</a>, based in Antwerp, Belgium.
+    </p>
+
+    <p>
+        Tracks are chosen and the radio show is presented by <a href="https://twitter.com/freekmurze">Freek Van der
+            Herten</a>.
+    </p>
+
+    <img class="img-responsive" src="/images/studio.jpg">
 
 @endsection
